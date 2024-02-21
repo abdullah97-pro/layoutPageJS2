2cf977a7bd71d877006ea3e06b9437410176ec83
abdullah97-pro <abdullahahmadi019@gmail.com>
2024-02-21 12:52:37 +0430

    create image and give css

--- js/script.js
+++ js/script.js
@@ -6,9 +6,16 @@ title.textContent = "Heading";
 var content = document.createElement("p");
 content.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio vel dolore alias animi architecto. Quia tempore magnam magni soluta et in qui eveniet incidunt, quasi veniam, nesciunt est deserunt nemo.";
 
+// create image
+var img = document.createElement("img");
+img.src = "https://th.bing.com/th?id=ORMS.d5b50e8b0096a2b49557eb668efac3e8&pid=Wdp&w=612&h=304&qlt=90&c=1&rs=1&dpr=1&p=0";
+img.alt = "New car";
+
 // get div container
 var container = document.getElementById('container');
 
 // include h1 tag into container
 container.appendChild(title);
 container.appendChild(content);
+container.appendChild(img);
+
